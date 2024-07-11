173069c75e1c3d4b8a115883ec416399f6d900ed
Marcel Putra <marcelino.ares.putra@gmail.com>
2024-07-11 14:11:08 +0800

    fixed property, broken loader

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 
-// function prototypes here
-void testPropertyClass();
+// headers 
+#include "property.h"
+#include "loader.h"
+#include "sorts.h"
 
 int main() {
-    int dataset_size;
-    std::string filename = "dataset.csv";
+    int dataset_size = 0;
+    Property** properties = Loader::loadProperties("properties.csv", dataset_size);
+
+    if (properties == nullptr) {
+        std::cerr << "Failed to load properties." << std::endl;
+        return 1;
+    }
+
+    // Use the properties array
+    for (int i = 0; i < dataset_size; i++) {
+        std::cout << "Property ID: " << properties[i]->getID() << ", Name: " << properties[i]->getName() << std::endl;
+        delete properties[i];  // Don't forget to delete each property object
+    }
+
+    delete[] properties;  // Finally, delete the array of pointers
 
     return 0;
-}
-
-void testPropertyClass() {
-
-}
+} 
--- property.h
+++ property.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <iostream>
-#include <fstream> 
-#include <sstream>
 #include <string>
 
 class Property {
@@ -18,45 +15,57 @@ private:
     int parking;
     int bathrooms;
     int size;
-
-    // standard arrays
+    std::string furnished;
     std::string* facilities;
+    int num_facilities;
     std::string* add_facilities;
-
+    int num_add_facilities;
     std::string region;
 
 public:
-    // constructor to build the objects on the fly
-    Property() {
+    // constructor
+    Property(int id, const std::string &name, int completion, int monthly_rent, const std::string &location, const std::string &type,
+             int rooms, int parking, int bathrooms, int size, const std::string &furnished,
+             const std::string *facilities, int num_facilities,
+             const std::string *add_facilities, int num_add_facilities,
+             const std::string &region) : id(id), name(name), completion(completion), monthly_rent(monthly_rent), location(location), type(type),
+                                          rooms(rooms), parking(parking), bathrooms(bathrooms), size(size), furnished(furnished),
+                                          num_facilities(num_facilities), num_add_facilities(num_add_facilities), region(region)
+    {
+        // allocate and set facilities
+        this->facilities = new std::string[num_facilities];
+        for (int i = 0; i < num_facilities; i++) {
+            this->facilities[i] = facilities[i];
+        }
 
+        // allocate and set additional facilities
+        this->add_facilities = new std::string[num_add_facilities];
+        for (int i = 0; i < num_add_facilities; i++) {
+            this->add_facilities[i] = add_facilities[i];
+        }
     }
 
-    // deconstructor
+    // deconstructor to free memory
     ~Property() {
         delete[] facilities;
         delete[] add_facilities;
     }
 
     // getters
-    int getID() { return id; };
-    std::string getName() { return name; };
-    int getCompletion() { return completion; };
-    int getMonthlyRent() { return monthly_rent; };
-    std::string getLocation() { return location; };
-    std::string getType() { return type; };
-    int getRooms() { return rooms; };
-    int getParking() { return parking; };
-    int getBathrooms() { return bathrooms; };
-    int getSize() { return size; };
-    std::string* getFacilities() { return facilities; };
-    std::string* getAddFacilities() { return add_facilities; };
-    std::string getRegion() { return region; };
-
-    // implement a loader here or as a separate class?
-
-    // print entry information in standard format
-    void display() {
-
-    }
-
+    int getID() const { return id; }
+    std::string getName() const { return name; }
+    int getCompletion() const { return completion; }
+    int getMonthlyRent() const { return monthly_rent; }
+    std::string getLocation() const { return location; }
+    std::string getType() const { return type; }
+    int getRooms() const { return rooms; }
+    int getParking() const { return parking; }
+    int getBathrooms() const { return bathrooms; }
+    int getSize() const { return size; }
+    std::string getFurnished() const { return furnished; }
+    const std::string* getFacilities() const { return facilities; }
+    int getNumFacilities() const { return num_facilities; }
+    const std::string* getAddFacilities() const { return add_facilities; }
+    int getNumAddFacilities() const { return num_add_facilities; }
+    std::string getRegion() const { return region; }
 };
